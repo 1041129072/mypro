93455809371eb86553da8e41b3d76f9ae259e879
wangkang <1041129072@qq.com>
2017-12-15 10:16:30 +0800

    change emp.java

--- emp.java
+++ emp.java
@@ -1,6 +1,8 @@
 import java.io.*;
+import java.util.*;
 public class Emp implements  Serializeable{
 	private integer empno;
 	private String ename;
 	private String job;
+	private Date  hiredate;
 }
