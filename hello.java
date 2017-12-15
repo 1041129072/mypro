4e1f8a75fc7e62839b60be6aa6012edc633d9a43
wangkang <1041129072@qq.com>
2017-12-15 08:59:19 +0800

    hello.java add new method

--- hello.java
+++ hello.java
@@ -2,5 +2,10 @@ public class hello{
 	public  static void main(String [] args){
 		System.out.println("Hello World!");
 		System.out.println("修改Hello World!");
+		print();
 	}
+	public static void print(){
+	System.out.println("Hello World!");
+	}
+	
 }
